2dc4d94319e01f6809948beb338c97bf613ff861
Guglielmo Frati <guglielmo.frati@codingduck.it>
2026-01-14 13:14:24 +0100

    Init

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+// vite.config.js
+import {resolve} from "path";
+import {defineConfig} from "vite";
+import dts from "vite-plugin-dts";
+
+const packageName = 'packageName';
+
+export default defineConfig({
+    plugins: [
+        dts({
+            insertTypesEntry: true,
+        }),
+    ],
+    build: {
+        lib: {
+            entry: resolve(__dirname, "src/index.ts"),
+            name: packageName,
+            // fileName: "index",
+            formats: ["es", "umd"],
+            fileName: (format) => `index.${format}.js`,
+        },
+    },
+});
